837e077f339ffd9ee4d8f64525dcb3ce7bb68d76
johms0976-cmyk <johms0976@gmail.com>
2026-01-20 21:33:19 +1300

    Create state.js

---
+++ docs/js/state.js
@@ -0,0 +1,20 @@
+import { showTitle } from "./title.js";
+import { showStart } from "./start.js";
+import { showMap } from "./map/mapRenderer.js";
+
+export const STATE = {
+  TITLE: "title",
+  START: "start",
+  MAP: "map"
+};
+
+export let currentState = STATE.TITLE;
+
+export function setState(state) {
+  currentState = state;
+  if (state === STATE.TITLE) showTitle();
+  if (state === STATE.START) showStart();
+  if (state === STATE.MAP) showMap();
+}
+
+setState(STATE.TITLE);
