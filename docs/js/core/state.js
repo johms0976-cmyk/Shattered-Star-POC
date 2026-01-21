f871391436ecc7632a7ea681a4551ea0ee564e8d
johms0976-cmyk <johms0976@gmail.com>
2026-01-21 13:54:09 +1300

    Create state.js

---
+++ docs/js/core/state.js
@@ -0,0 +1,17 @@
+export const STATE = {
+  TITLE: "title",
+  START: "start",
+  MAP: "map"
+};
+
+export const GameState = {
+  current: STATE.TITLE,
+  run: {
+    map: null,
+    currentNode: null
+  }
+};
+
+export function setState(state) {
+  GameState.current = state;
+}
