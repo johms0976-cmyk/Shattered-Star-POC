e9cfc1fb5b26e73c5536028ddc07e45e969e913e
johms0976-cmyk <johms0976@gmail.com>
2026-01-21 21:30:10 +1300

    Update transitions.js

--- docs/js/core/transitions.js
+++ docs/js/core/transitions.js
@@ -1,2 +1,7 @@
+// js/core/transitions.js
+//
+// Unified transition API for Shattered Star.
+// Re-exports both canvas and UI transition systems.
+
 export * from "./transitionsCanvas.js";
 export * from "./transitionsUI.js";
