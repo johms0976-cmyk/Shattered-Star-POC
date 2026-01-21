faa77a351ed5bfb17959549df89b39eefe17ab41
johms0976-cmyk <johms0976@gmail.com>
2026-01-21 18:56:44 +1300

    Update manifest.js

--- docs/assets/manifest.js
+++ docs/assets/manifest.js
@@ -2,11 +2,13 @@
 
 export const ASSET_MANIFEST = [
   // Title animation frames
-  "assets/title/title1.png",
-  "assets/title/title2.png",
-  "assets/title/title3.png",
-  "assets/title/title4.png",
-  "assets/title/title5.png",
+  "assets/screens/title/title1.png",
+  "assets/screens/title/title2.png",
+  "assets/screens/title/title3.png",
+  "assets/screens/title/title4.png",
+  "assets/screens/title/title5.png",
+  "assets/screens/new-game/newgame1.png",
+  "assets/screens/start/startscreen1.png"
 
   // Add more assets here:
   // "assets/heroes/korvax.png",
