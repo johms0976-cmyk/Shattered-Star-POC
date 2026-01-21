420c2181b24529662a9f920cdda240641d571fa3
johms0976-cmyk <johms0976@gmail.com>
2026-01-21 14:12:59 +1300

    Update mainMain.js

--- docs/js/systems/ui/mainMain.js
+++ docs/js/systems/ui/mainMain.js
@@ -1,1 +1,23 @@
+// js/ui/mainMenu.js
+
+import { setState, STATE } from "../core/state.js";
+import { applyState } from "../core/stateRouter.js";
+
+let initialized = false;
+
+export function showStart() {
+  const screen = document.getElementById("start-screen");
+  screen.classList.add("active");
+
+  if (initialized) return;
+
+  const newGameBtn = document.getElementById("new-game");
+
+  newGameBtn.onclick = () => {
+    setState(STATE.MAP);
+    applyState();
+  };
+
+  initialized = true;
+}
 
