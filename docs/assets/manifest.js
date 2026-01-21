8b73b9522d0807149ddc93f8bd5a8db73714f985
johms0976-cmyk <johms0976@gmail.com>
2026-01-21 18:54:54 +1300

    Create manifest.js

---
+++ docs/assets/manifest.js
@@ -0,0 +1,15 @@
+// assets/manifest.js
+
+export const ASSET_MANIFEST = [
+  // Title animation frames
+  "assets/title/title1.png",
+  "assets/title/title2.png",
+  "assets/title/title3.png",
+  "assets/title/title4.png",
+  "assets/title/title5.png",
+
+  // Add more assets here:
+  // "assets/heroes/korvax.png",
+  // "assets/ui/button.png",
+  // "assets/enemies/beast.png",
+];
