72c7adca4e8a394c3385bd6f8f2586d058b9c330
johms0976-cmyk <johms0976@gmail.com>
2026-01-21 19:58:10 +1300

    Update manifest.js

--- docs/assets/manifest.js
+++ docs/assets/manifest.js
@@ -1,17 +1,42 @@
 // assets/manifest.js
 
 export const ASSET_MANIFEST = [
-  // Title animation frames
+
+  // ------------------------------------------------------------
+  // TITLE SCREEN ANIMATION
+  // ------------------------------------------------------------
   "assets/screens/title/title1.png",
   "assets/screens/title/title2.png",
   "assets/screens/title/title3.png",
   "assets/screens/title/title4.png",
   "assets/screens/title/title5.png",
-  "assets/screens/new-game/newgame1.png",
-  "assets/screens/start/startscreen1.png"
 
-  // Add more assets here:
-  // "assets/heroes/korvax.png",
+  // ------------------------------------------------------------
+  // START SCREEN
+  // ------------------------------------------------------------
+  "assets/screens/start/startscreen1.png",
+
+  // ------------------------------------------------------------
+  // NEW GAME SCREEN
+  // ------------------------------------------------------------
+  "assets/screens/new-game/newgame1.png",
+
+  // ------------------------------------------------------------
+  // (Add more assets below as needed)
+  // ------------------------------------------------------------
+  // UI elements
   // "assets/ui/button.png",
+  // "assets/ui/panel.png",
+
+  // Heroes
+  // "assets/heroes/korvax.png",
+  // "assets/heroes/lyria.png",
+
+  // Maps
+  // "assets/maps/act1_surface.png",
+  // "assets/maps/act2_corrupted.png",
+
+  // Enemies
   // "assets/enemies/beast.png",
+  // "assets/enemies/voidling.png",
 ];
