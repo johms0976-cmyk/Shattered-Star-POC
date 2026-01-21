07acbaba825d4846cf068efeba01f501b138c282
johms0976-cmyk <johms0976@gmail.com>
2026-01-21 19:55:20 +1300

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,32 +1,24 @@
 // main.js
 
-// ------------------------------------------------------------
-// CORE ENGINE IMPORTS
-// ------------------------------------------------------------
-import "./js/core/state.js";
-import "./js/core/stateRouter.js";
-import "./js/core/renderer.js";
-import "./js/core/transitions.js";
-import "./js/core/crt.js";
+// CORE
+import { setState, STATE } from "./js/core/state.js";
+import { applyState } from "./js/core/stateRouter.js";
+import { preloadAssets } from "./js/core/loader.js";
+import { updateLoadingProgress } from "./js/ui/loadingScreen.js";
 
-// ------------------------------------------------------------
-// UI SCREENS
-// ------------------------------------------------------------
+// UI (side-effect imports to register listeners / logic)
 import "./js/ui/titleScreen.js";
 import "./js/ui/mainMenu.js";
 import "./js/ui/newGame.js";
 import "./js/ui/loadingScreen.js";
+import "./js/ui/settingsMenu.js";
+import "./js/ui/actIntro.js";
 
-// ------------------------------------------------------------
-// GAME SYSTEMS
-// ------------------------------------------------------------
-
-// MAP
+// SYSTEMS
 import "./js/systems/map/mapUI.js";
 import "./js/systems/map/mapCanvasScene.js";
 import "./js/systems/map/mapGenerator.js";
 
-// COMBAT
 import "./js/systems/combat/combatUI.js";
 import "./js/systems/combat/engine.js";
 import "./js/systems/combat/actions.js";
@@ -34,30 +26,18 @@ import "./js/systems/combat/statusEffects.js";
 import "./js/systems/combat/enemyAI.js";
 
 // ------------------------------------------------------------
-// ASSET PRELOADER
-// ------------------------------------------------------------
-import { preloadAssets } from "./js/core/loader.js";
-import { updateLoadingProgress } from "./js/ui/loadingScreen.js";
-
-// ------------------------------------------------------------
-// STATE MACHINE
-// ------------------------------------------------------------
-import { setState, STATE } from "./js/core/state.js";
-import { applyState } from "./js/core/stateRouter.js";
-
-// ------------------------------------------------------------
-// GAME BOOTSTRAP
+// BOOT FLOW
 // ------------------------------------------------------------
 
-// Step 1: Show loading screen immediately
+// 1. Show loading screen
 setState(STATE.LOADING);
 applyState();
 
-// Step 2: Begin preloading assets
+// 2. Preload assets
 preloadAssets(progress => {
   updateLoadingProgress(progress);
 }).then(() => {
-  // Step 3: When done, go to title screen
+  // 3. When done, go to title
   setState(STATE.TITLE);
   applyState();
 });
