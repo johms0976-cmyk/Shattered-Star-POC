b56e8a1061940fcda4e61ada5c5e59abdc26bd36
johms0976-cmyk <johms0976@gmail.com>
2026-01-21 14:23:32 +1300

    Update stateRouter.js

--- docs/js/core/stateRouter.js
+++ docs/js/core/stateRouter.js
@@ -1,40 +1,44 @@
 // js/core/stateRouter.js
 
 import { STATE, GameState } from "./state.js";
+import { glitchTransition } from "./transition.js";
 
 import { showTitle } from "../ui/titleScreen.js";
 import { showStart } from "../ui/mainMenu.js";
+import { showNewGame } from "../ui/newGameScreen.js";
 import { showMap } from "../systems/map/mapUI.js";
+import { showCombat } from "../systems/combat/combatUI.js";
 
-/**
- * The ONLY place that decides which screen is shown.
- */
 export function applyState() {
-  hideAllScreens();
+  glitchTransition(() => {
+    hideAllScreens();
 
-  switch (GameState.current) {
-    case STATE.TITLE:
-      showTitle();
-      break;
+    switch (GameState.current) {
+      case STATE.TITLE:
+        showTitle();
+        break;
 
-    case STATE.START:
-      showStart();
-      break;
+      case STATE.START:
+        showStart();
+        break;
 
-    case STATE.MAP:
-      showMap();
-      break;
+      case STATE.NEWGAME:
+        showNewGame();
+        break;
 
-    default:
-      console.warn("Unknown state:", GameState.current);
-      showTitle();
-      break;
-  }
-}
+      case STATE.MAP:
+        showMap();
+        break;
 
-function hideAllScreens() {
-  document.querySelectorAll(".screen").forEach(screen => {
-    screen.classList.remove("active");
+      case STATE.COMBAT:
+        showCombat();
+        break;
+    }
   });
 }
 
+function hideAllScreens() {
+  document.querySelectorAll(".screen").forEach(s =>
+    s.classList.remove("active")
+  );
+}
