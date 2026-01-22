095c9d2922d93062ccd7d6c5ffb952a9aec95144
johms0976-cmyk <johms0976@gmail.com>
2026-01-22 21:35:32 +1300

    Delete docs/js/transitions.js

--- docs/js/transitions.js
+++
@@ -1,8 +0,0 @@
-export function transition(callback) {
-  const screen = document.getElementById("screen");
-  screen.classList.add("glitch");
-  setTimeout(() => {
-    screen.classList.remove("glitch");
-    callback();
-  }, 300);
-}
